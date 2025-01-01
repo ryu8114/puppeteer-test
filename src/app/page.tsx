093d889790cdcc66745f89affd843d892a2d3682
RyuTanaka <nogardwons@gmail.com>
2025-01-01 20:18:23 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,15 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [imageData, setImageData] = useState("");
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      try {
-        const response = await fetch('/api/test');
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setImageData(data.image);
-      } catch (error) {
-        setError(error.message);
+      const response = await fetch('/api/test');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
+      const data = await response.json();
+      setImageData(data.image);
     }
     fetchData();
   }, []);
@@ -25,7 +20,7 @@ export default function Home() {
   return (
     <main>
       <h1>スクリーンショット</h1>
-      {error ? <p>Error: {error}</p> : <img src={`data:image/png;base64,${imageData}`} alt="Screenshot" />}
+      <img src={`data:image/png;base64,${imageData}`} alt="Screenshot" />
     </main>
   )
 }
